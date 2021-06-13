ef3b5e71bbd55e32fbc099cd57c61e27a2061b7a
Manolis Tzanidakis <mtzanidakis@gmail.com>
2021-06-14 01:38:44 +0300

    add set -e; merge subshells

--- bin/vpnbrowser.sh
+++ bin/vpnbrowser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -e
 pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
 xhost +local:
-(cd $HOME/.local/vpnproxy; docker-compose up)
-(cd $HOME/.local/vpnproxy; docker-compose down)
+(cd $HOME/.local/vpnproxy; docker-compose up; docker-compose down)
 xhost -
 pactl unload-module $(pactl list short modules | awk '/\/tmp\/pulseaudio.socket/ { print $1 }')
