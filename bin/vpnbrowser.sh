9a5b4cbd7d9a73347aecfef30cd5ed6bff4da7d7
Manolis Tzanidakis <mtzanidakis@gmail.com>
2021-06-14 02:31:10 +0300

    add multi-country support

--- bin/vpnbrowser.sh
+++ bin/vpnbrowser.sh
@@ -2,6 +2,10 @@
 set -e
 pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
 xhost +local:
-(cd $HOME/.local/vpnproxy; docker-compose up; docker-compose down)
+(cd $HOME/.local/vpnproxy;
+export COUNTRY=${1:-gre};
+envsubst < vpn/vpn.conf.template > vpn/vpn.conf;
+docker-compose up; docker-compose down;
+rm -f vpn/vpn.conf)
 xhost -
 pactl unload-module $(pactl list short modules | awk '/\/tmp\/pulseaudio.socket/ { print $1 }')
