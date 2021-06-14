029c335382424a6a74e366d16066cd8d9f28accb
Manolis Tzanidakis <mtzanidakis@gmail.com>
2021-06-14 17:32:41 +0300

    switch to wireguard

--- bin/vpnbrowser.sh
+++ bin/vpnbrowser.sh
@@ -2,10 +2,6 @@
 set -e
 pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
 xhost +local:
-(cd $HOME/.local/vpnproxy;
-export COUNTRY=${1:-gre};
-envsubst < vpn/vpn.conf.template > vpn/vpn.conf;
-docker-compose up; docker-compose down;
-rm -f vpn/vpn.conf)
+(cd $HOME/.local/vpnproxy; docker-compose up; docker-compose down)
 xhost -
 pactl unload-module $(pactl list short modules | awk '/\/tmp\/pulseaudio.socket/ { print $1 }')
