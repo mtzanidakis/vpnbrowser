1b3d1fbf776e0aa47a11f58ca1e3115fe3ea53ce
Manolis Tzanidakis <mtzanidakis@gmail.com>
2021-06-18 19:56:20 +0300

    fix path

--- bin/vpnbrowser.sh
+++ bin/vpnbrowser.sh
@@ -2,6 +2,6 @@
 set -e
 pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
 xhost +local:
-(cd $HOME/.local/vpnproxy; docker-compose up; docker-compose down)
+(cd $HOME/.local/vpnbrowser; docker-compose up; docker-compose down)
 xhost -
 pactl unload-module $(pactl list short modules | awk '/\/tmp\/pulseaudio.socket/ { print $1 }')
