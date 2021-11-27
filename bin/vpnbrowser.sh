701ffab4c647b35684561072b32f0980a4ca48d0
Manolis Tzanidakis <mtzanidakis@gmail.com>
2021-11-27 02:08:17 +0200

    remove pactl (un)load-module; switch to pipewire

--- bin/vpnbrowser.sh
+++ bin/vpnbrowser.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
 xhost +local:
 (cd $HOME/.local/vpnbrowser; docker-compose up; docker-compose down)
 xhost -
-pactl unload-module $(pactl list short modules | awk '/\/tmp\/pulseaudio.socket/ { print $1 }')
