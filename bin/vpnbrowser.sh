50de3550c48265937c1a2c57da903612d1a057f9
Manolis Tzanidakis <mtzanidakis@gmail.com>
2022-01-12 19:37:30 +0200

    improve start/stop; handle docker-compose v2 properly

--- bin/vpnbrowser.sh
+++ bin/vpnbrowser.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
 set -e
-xhost +local:
-(cd $HOME/.local/vpnbrowser; docker-compose up; docker-compose down)
-xhost -
+
+_dc="docker compose --project-directory $HOME/.local/vpnbrowser"
+
+function start() {
+	xhost +local:
+	${_dc} up -d
+}
+
+function stop() {
+	${_dc} down
+	xhost -
+	exit 0
+}
+
+# trap ctrl+c
+trap stop INT
+
+start
+echo -e "\n\033[0;32mpress ctrl+c to exit \n\033[m"
+sleep infinity
